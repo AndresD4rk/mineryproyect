7d66d1a3992167261120a74d7a722cb23e7071e1
AndresD4rk <andresfb053@gmail.com>
2023-05-12 00:26:46 -0500

    UwU

--- analisis.php
+++ analisis.php
@@ -1,13 +1,23 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/base.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</head>
+<body>
+
 <?php
 session_start();
 
 if (!isset($_SESSION["archivo"])) {
-  // Si el identificador del archivo no está en la sesión, redirigir de vuelta a la página de carga
+  // Si el identificador del archivo no estï¿½ en la sesiï¿½n, redirigir de vuelta a la pï¿½gina de carga
   header("Location: upload.php");
   exit;
 }
 
-// Obtener el identificador del archivo de la sesión
+// Obtener el identificador del archivo de la sesiï¿½n
 $identificador = $_SESSION["archivo"];
 
 // Obtener la ruta del archivo temporal en el servidor
@@ -16,40 +26,71 @@ $ruta_archivo = "tmp/" . $identificador;
 // Leer el contenido del archivo
 $contenido = file_get_contents($ruta_archivo);
 
-// Resto del código para analizar el archivo
+// Resto del cï¿½digo para analizar el archivo
 // ...
 
-// Limpiar la sesión para que se pueda cargar otro archivo
+// Limpiar la sesiï¿½n para que se pueda cargar otro archivo
 
 
-  // Encontrar la posición de la cadena de búsqueda
+  // Encontrar la posiciï¿½n de la cadena de bï¿½squeda
   $posicion = strpos($contenido, "Volcado de datos para la tabla");
 
-  // Obtener el contenido a partir de la posición
+  // Obtener el contenido a partir de la posiciï¿½n
   $contenido = substr($contenido, $posicion);
 
-  // Dividir el contenido en líneas
+  // Dividir el contenido en lï¿½neas
   $lineas = explode("\n", $contenido);
 
   // Obtener los nombres de las columnas
   $columnas = str_getcsv($lineas[0], ",");
 
-  // Obtener el índice de la columna seleccionada por el usuario
+  // Obtener el ï¿½ndice de la columna seleccionada por el usuario
   $columna_seleccionada = $_POST['columna'];
 
   // Imprimir una tabla HTML con los datos de la columna seleccionada
-  echo "<table class='mi-tabla'>";
-  echo "<tr>";
-  echo "<th>" . htmlspecialchars($columnas[$columna_seleccionada]) . "</th>"; 
-  echo "</tr>";
+  echo "<div style='max-height: 300px;overflow-y: auto;'>
+  <table class='table table-dark table-hover'>";;
+  
   foreach($lineas as $i => $linea) {
+    echo "<tr>";
     $campos = str_getcsv($linea, ",");
     if (isset($campos[$columna_seleccionada])) {
-      echo "<tr>";
-      echo "<td>" . htmlspecialchars(stripcslashes($campos[$columna_seleccionada])) . "</td>";
-      echo "</tr>";
-    }
+      if ($i === 0) {
+        echo "<th>" . htmlspecialchars($campos[$columna_seleccionada]) . "</th>"; 
+      } else {
+        echo "<td>" . htmlspecialchars(stripcslashes($campos[$columna_seleccionada])) . "</td>";
+
+      }
+      
+    }echo "</tr>";
   }
   echo "</table>";
+echo "<br><br>";
+
+$valores = array();
+
+foreach($lineas as $i => $linea) {
+  $campos = str_getcsv($linea, ",");
+  if (isset($campos[$columna_seleccionada])) {
+    if ($i > 0) { // Ignorar la primera fila (encabezados de columna)
+      // Agregar el valor al array de valores
+      $valores[] = $campos[$columna_seleccionada];
+    }
+  }
+}
+
+// Contar la frecuencia de cada valor en el array de valores
+$frecuencias = array_count_values($valores);
+
+// Imprimir una tabla HTML con las frecuencias de los valores en la columna seleccionada
+echo "<table>";
+echo "<tr><th>Valor</th><th>Frecuencia</th></tr>";
+foreach ($frecuencias as $valor => $frecuencia) {
+  echo "<tr><td>" . htmlspecialchars($valor) . "</td><td>" . htmlspecialchars($frecuencia) . "</td></tr>";
+}
+echo "</table>";
 
 ?>
+
+</body>
+</html>
--- upload.php
+++ upload.php
@@ -2,30 +2,9 @@
 
 <html>
 <head>
-  <style>
-    table {
-      border-collapse: collapse;
-      width: 100%;
-      max-height: 300px;
-        overflow-y: auto;
-      
-    }
-
-    th, td {
-      padding: 8px;
-      text-align: left;
-      border-bottom: 1px solid #ddd;
-    }
-
-    th {
-      background-color: #4CAF50;
-      color: white;
-    }
-    div {
- max-height: 300px;
-  overflow-y: auto;
-}
-  </style>
+<link rel="stylesheet" type="text/css" href="css/base.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 </head>
 <body>
  
@@ -51,7 +30,8 @@
     $columnas = str_getcsv($lineas[0], ",");
 
     // Imprimir una tabla HTML con el contenido del archivo y agregar un atributo de clase
-    echo "<div><table class='mi-tabla'>";
+    echo "<div style='max-height: 300px;overflow-y: auto;'>
+    <table class='table table-dark table-hover'>";
     foreach($lineas as $i => $linea) {
       $campos = str_getcsv($linea, ",");
       echo "<tr>";
