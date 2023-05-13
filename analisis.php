9bccad969149e8b76355d7dfa3f03f2ef8761b91
AndresD4rk <andresfb053@gmail.com>
2023-05-13 02:32:01 -0500

    12345

--- analisis.php
+++ analisis.php
@@ -3,9 +3,9 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="css/base.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+ --><script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 </head>
 <body>
@@ -131,10 +131,6 @@ $frecuencias = array_values($frecuencias);
 $colores = array('#FF6384', '#36A2EB', '#FFCE56', '#8BC34A', '#9C27B0');
 ?>
 <script>
-// Obtener el elemento canvas
-var canvas = document.getElementById('grafica-torta');
-
-
 // Obtener el elemento canvas
 var canvas = document.getElementById('grafica-torta');
 
